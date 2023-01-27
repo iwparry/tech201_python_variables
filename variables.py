b980e547293ab47fdf4a59b7f909732946b16a96
iwparry <iwparry@hotmail.co.uk>
2023-01-27 15:56:38 +0000

    Adding user input task

---
+++ variables.py
@@ -0,0 +1,38 @@
+# Variables
+
+# variable_name = variable_data
+
+a = 1 #int
+b = 2 #int
+c = 3.5 #float
+hi = "Hello World!" #str
+# comment multiple lines simultaneously 'Ctrl + /'
+
+#print(hi)
+#print(a)
+#print(a + b)
+
+# the type method
+
+#print(type(hi))
+# returns <class 'str'>
+
+#print(a)
+#a=4
+#print(a)
+
+# Getting user input
+
+#print('What is your name?')
+#name = input()
+#print('Hi')
+#print(name)
+
+# Make a program that takes user's name, age and DOB and prints them back out
+print('What is your name, age and date of birth?')
+name = input()
+age = input()
+dob = input()
+print('Name: ' + name)
+print('Age ' + age)
+print('Date of birth: ' + dob)
